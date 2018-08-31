fe87b43de6d8a8b4f2fe18ae96abbabbc854dd1c
MrDallOca <tdalloca@gmail.com>
2018-08-31 10:03:02 -0300

    Completed lab02

--- 02-esteganografia/esteganografia.c
+++ 02-esteganografia/esteganografia.c
@@ -4,19 +4,14 @@ int main(){
 	char p2[3];
 	int h,w,maxg;
 	scanf("%2s %d %d %d", p2, &h, &w, &maxg);
-
-	unsigned int M[h][w];
-	unsigned char mask = 0x03; // 00000011
-	for(int i = 0; i < h; ++i)
-		for(int j = 0; j < w; ++j)
-			scanf("%u", &M[i][j]);
-	
-	unsigned char frase[1000];
+	unsigned char frase[100000];
 	unsigned char letra = 0x0;
 	int k = 1;
 	for(int i = 0; i < h; ++i)
 		for(int j = 0; j < w; ++j){
-			unsigned char last_bits = mask & M[i][j];
+			unsigned int u;
+			scanf("%u", &u);
+			unsigned char last_bits = 0x03 & u;
 			if (k <= 4){
 				letra <<= 2;
 				letra |= last_bits;
